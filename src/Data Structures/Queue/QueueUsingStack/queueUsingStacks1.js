80d0d2cff5f3c0ca70c812603b02174bba2100b5
Hani <ummehanimohammed9@gmail.com>
2023-04-01 02:37:02 +0530

    Implement a Queue using Stacks
    - In this solution, enqueue operation is optimized and dequeue is expensive.
    - Time complexity of enqueue is O(1) as we simply push to the main stack
    - Time complexity of dequeue is O(n) as we transfer elements between stacks two times.

--- src/Data Structures/Queue/QueueUsingStack/queueUsingStacks1.js
+++ src/Data Structures/Queue/QueueUsingStack/queueUsingStacks1.js
@@ -2,28 +2,32 @@
 const Stack = require('../../Stack/stack')
 
 //Time Complexity
-//enqueue  - O(n)
-//dequeue  - O(1)
+//enqueue  - O(1)
+//dequeue  - O(n)
 
-class queueUsingStacks2 {
+class queueUsingStack {
     constructor() {
         this.mainStack = new Stack()
         this.tempStack = new Stack()
     }
 
     enqueue(value) {
-        while(!this.mainStack.isEmpty()) {
-            this.tempStack.push(this.mainStack.pop())
-        }
         this.mainStack.push(value)
-        while(!this.tempStack.isEmpty()) {
-            this.mainStack.push(this.tempStack.pop())
-        }
+        return true
     }
 
     dequeue() {
-        if(this.mainStack.isEmpty()) return null
-        return this.mainStack.pop()
+        if(this.mainStack.isEmpty()) 
+            return null
+
+        while(!this.mainStack.isEmpty()) {
+            this.tempStack.push(this.mainStack.pop())
+        }
+        const element = this.tempStack.pop()
+        while(!this.tempStack.isEmpty()) {
+            this.mainStack.push(this.tempStack.pop())
+        }
+        return element
     }
 
     print() {
@@ -31,12 +35,15 @@ class queueUsingStacks2 {
     }
 }
 
-const queue = new queueUsingStacks2()
+const queue = new queueUsingStack()
 
 for(var i=0;i<5;i++)
     queue.enqueue(i+1)
+
 queue.print()
+
 console.log("----------");
+
 console.log(queue.dequeue())
 queue.print()
 console.log(queue.dequeue())
@@ -45,6 +52,8 @@ queue.enqueue(10)
 queue.print()
 console.log(queue.dequeue())
 queue.print()
+
+
 // Sample Input
 // value = 5 
 // //[1, 2, 3, 4] existing queue front:1 back:4
@@ -52,5 +61,5 @@ queue.print()
 // dequeue()
 
 // Sample Output
-//[1, 2, 3, 4, 5]
+// True //[1, 2, 3, 4, 5]
 // 1 //[2, 3, 4, 5]
